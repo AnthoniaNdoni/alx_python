13ba70e19c4d9cbde2c8aac787947f1af1767f68
AnthoniaNdoni <tamarandoni@gmail.com>
2023-07-22 01:48:09 +0100

    my first commit

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -3,7 +3,6 @@ word = "Holberton"
 word_last_3 = word [:3]
 word_last_2 = word [7:]
 middle_word = word [0:7]
-middle_word = "olberto"
 print("First 3 letters: {}".format(word_last_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
