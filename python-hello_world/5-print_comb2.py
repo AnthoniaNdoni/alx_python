43d8476c24d05fc4d48915f90f7243fc0eeb9d98
AnthoniaNdoni <tamarandoni@gmail.com>
2023-07-24 22:54:05 +0100

    updated commit

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -4,9 +4,3 @@ for num in range(0, 100):
         print("0{}".format(num), end=", ")
     else:
         print("{}, ".format(num) if num < 99 else "{}\n".format(num), end="") 
-#!/usr/bin/python3
-for num in range(0, 100):
-    if num <= 9:
-        print("0{}".format(num), end=", ")
-    else:
-        print("{}, ".format(num) if num < 99 else "{}".format(num), end="") 
