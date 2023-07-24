4ac1fe638678750ac54bd64a460115e664a11cfb
AnthoniaNdoni <tamarandoni@gmail.com>
2023-07-24 20:00:11 +0100

    commit num

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for num in range(10):
-    for e in range(9):
-        print(f"{num}{e},", end=" ")
-        print("99\n") 
+for num in range(0, 100):
+    if num <= 9:
+        print("0{}".format(num), end=", ")
+    else:
+        print(f"{num}," if num < 99 else f"{num}\n", end=" ") 
