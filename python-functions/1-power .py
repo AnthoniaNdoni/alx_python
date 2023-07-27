83b10254b6cfde170f8717cb0c18fedd35bbfc2f
AnthoniaNdoni <tamarandoni@gmail.com>
2023-07-28 00:58:19 +0100

    function that computes a to the power of b

--- python-functions/1-power .py
+++ python-functions/1-power .py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def pow(a, b):
-     result = 1
-
-     for _ in range(b):
+     if b  == 0:
+          return 1
+     result = a
+     for _ in range(1, b):
           result *= a
+
      return result
 
 print(pow(2, 2))
