966dfc61b1f19d2c8ff6ec074b1ea283672e18c6
AnthoniaNdoni <125657089+AnthoniaNdoni@users.noreply.github.com>
2023-07-28 11:35:14 -0400

    Update 1-power.py

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 def pow(a, b):
-    result = pow(a, b)
-    return result
+    if b == 0:
+        return 1
+    elif b > 0:
+        mul = 1
+        for _ in range(b):
+            mul = mul * a
+        return result
+    else:
+        return 1 / pow(a, -b)
