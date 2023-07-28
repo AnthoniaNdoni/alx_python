1b4dca54bd9d612b7fdaecdbdf3674e9b455256a
AnthoniaNdoni <tamarandoni@gmail.com>
2023-07-28 01:32:19 +0100

    function convert_to_celsius

--- python-functions/2-temperature .py
+++ python-functions/2-temperature .py
@@ -3,6 +3,10 @@ def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5 / 9
     return celsius 
 
+temperature_fahrenheit = 68
+temperature_celsius = convert_to_celsius(temperature_fahrenheit)
+print(f"{temperature_fahrenheit}°F is equal to {temperature_celsius:.2f}°C")
+
 print(convert_to_celsius(100))
 print(convert_to_celsius(-40))
 print(convert_to_celsius(-459.67))
