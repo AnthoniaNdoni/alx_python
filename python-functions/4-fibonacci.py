a5494359d6581ce66d0006fca7fd2b16edad6afb
AnthoniaNdoni <tamarandoni@gmail.com>
2023-07-29 22:02:44 +0100

    fibonacci sequence commit

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    else:
+     sequence = [0,1]
+    while len(sequence) < n:
+        sequence.append(sequence[-1] + sequence[-2])
+        return sequence
