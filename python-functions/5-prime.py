0fe382f65677817ece7cb9b229388f872576a038
AnthoniaNdoni <tamarandoni@gmail.com>
2023-07-31 00:16:18 +0100

    prime number

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -4,12 +4,14 @@ def is_prime(number):
         return False
     if number == 2:
         return True
+    
     for i in range(2, int(number ** 0.5) +1):
         if number % i == 0:
             return False
+        
     return True
-
-result = is_prime(17)
+if __name__ == "__main__":
+    result = is_prime(17)
 print(result)
 
 result = is_prime(4)
