c6ac5123fb1d94679b2808928b9387ee2af42046
AnthoniaNdoni <tamarandoni@gmail.com>
2023-07-30 23:02:59 +0100

    prime number commit

---
+++ python-functions/5-prime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number ** 0.5) +1):
+        if number % i == 0:
+             return False
+    return True
+
+result = is_prime(17)
+print(result)
