56c2f9465e6aeef3a592f029bf1d7ea087bac992
AnthoniaNdoni <tamarandoni@gmail.com>
2023-07-30 18:26:33 +0100

    edited commit

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 def fibonacci_sequence(n):
-    if n <= 0:
-        return []
-    elif n == 1:
-        return [0]
-    else:
-        sequence = [0,1,1,2,3,5]
-    while len(sequence) < n:
-        sequence.append(sequence[-1] + sequence[-2])
+    sequence = [0,1]
+    if n == 0:
+        sequence = []
+    if n == 1:
+        sequence = [0]
+    while Len(sequence)<n:
+        adder = sequence[-1] + sequence[-2]
         return sequence
