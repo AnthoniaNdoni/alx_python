e3fa3692ba0f1753b2b7b71d6c22130e15261c4e
AnthoniaNdoni <tamarandoni@gmail.com>
2023-07-30 14:36:58 +0100

    edit

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -5,7 +5,7 @@ def fibonacci_sequence(n):
     elif n == 1:
         return [0]
     else:
-     sequence = [0,1]
+        sequence = [0,1,1,2,3,5]
     while len(sequence) < n:
         sequence.append(sequence[-1] + sequence[-2])
         return sequence
