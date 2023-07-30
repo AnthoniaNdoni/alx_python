fe3333cf674a88dbc8fc564b5fe1074a6372d6df
AnthoniaNdoni <125657089+AnthoniaNdoni@users.noreply.github.com>
2023-07-30 15:51:36 -0400

    fixed logic in  4-fibonacci.py

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 def fibonacci_sequence(n):
+    sequence = [0, 1]
     if n <= 0:
-        return []
-    elif n == 1:
-        return [0]
+        sequence = []
+    if n == 1:
+        sequence = [0]
     else:
-        sequence = [0, 1]
-    while len(sequence) < n:
-        sequence.append(sequence[-1] + sequence[-2])
-        return sequence
+        while len(sequence) < n:
+            added = sequence[-1] + sequence[-2]
+            sequence.append(added)
+    return sequence
