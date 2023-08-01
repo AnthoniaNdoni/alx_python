e98879f1320200cdca8d3c0c056a17915a77c7ce
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-01 23:45:24 +0100

    editing import func commit

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -6,7 +6,6 @@ def fibonacci_sequence(n):
     if n == 1:
         sequence = [0]
     else:
-<<<<<<< HEAD
         sequence = [0, 1]
     while len(sequence) < n:
         sequence.append(sequence[-1] + sequence[-2])
@@ -15,9 +14,3 @@ def fibonacci_sequence(n):
 
     print(fibonacci_sequence(6))
     print(fibonacci_sequence(1))
-=======
-        while len(sequence) < n:
-            added = sequence[-1] + sequence[-2]
-            sequence.append(added)
-    return sequence
->>>>>>> fe3333cf674a88dbc8fc564b5fe1074a6372d6df
