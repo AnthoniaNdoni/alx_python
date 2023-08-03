5ac9c4e4686fa0cebd1f196a175aa71ebe600582
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-03 23:31:54 +0100

    integers division commit

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("cannot divide by zero.")
+        return None
+    else:
+        print("{}".format(result))
+        return result
+    finally:
+        print()
+
+result = safe_print_division(12, 2)
