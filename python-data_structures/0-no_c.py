11f0cabeea14518ebdf18990f085782e861c411b
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-06 22:00:12 +0100

    editing commit

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     return ''.join(char for char in my_string if char.lower() not in ('c', 'Ç'))
-
-original_string = "Holberton school!"
-result = no_c(original_string)
-print(result)
