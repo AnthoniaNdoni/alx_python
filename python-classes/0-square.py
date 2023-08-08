f1e4b4bc6cca0e815d23ed975a59e20ef3933d60
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-08 23:48:05 +0100

    square commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-class Square:
+class Square: # Represents a square with a given size
+        # Attributes: 
+        # __size (int): The size of square.
+        # Methods: __init__(salf, size)
     def __init__(self, size):
+        #  Initializes a new Square instance with the specified size.
+        #Args:
+        #   size (int): The size of the square.
+        # Returns:
+        #  None
         self.__size = size 
+
