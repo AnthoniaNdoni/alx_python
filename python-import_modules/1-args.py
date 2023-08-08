b248b4b9a0f701676b96d673bf759463a1c21e6a
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-08 23:19:29 +0100

    commit

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,12 +1,13 @@
 if __name__ == "__main__" :
     import sys
+
     if len(sys.argv) == 1:
-        print("0 arguments.")
+        print("0 arguments")
     elif len(sys.argv) == 2:    
-        print("1 argument:\n1: {}".format(sys.argv[1]))
+        print("1 argument\n1: {}".format(sys.argv[1]))
     else:
         count = 1
-        print("{} arguments:".format(len(sys.argv) -1))
+        print("{} argument(s)".format(len(sys.argv) -1))
         for i in range(1, len(sys.argv)):
                 print("{}: {}".format(count, sys.argv[i]))
                 count += 1
