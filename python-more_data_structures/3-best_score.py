be90a2e36fa89c659bac3429548d9a7ad6179b38
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-08 14:12:36 +0100

    editing commit

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -7,6 +7,6 @@ def best_score(a_dictionary):
     return max_key
 
 
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'John': 16, 'Adam': 10}
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
 result = best_score(a_dictionary)
 print("Best:", result)
