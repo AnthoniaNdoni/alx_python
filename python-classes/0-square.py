0700b4f1f333750983e7047a64e974fb079fd3d4
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-08 22:34:09 +0100

    editing commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -3,8 +3,5 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
-square1 = Square('0-square')
+square1 = Square(3)
 square2 = Square(3)
-
-print(type(square1))
-print(square2.__dict__)
