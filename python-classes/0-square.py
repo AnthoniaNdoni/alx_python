5aa5b6ab8fb55e9b1c2359f3b78a875add1101d3
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-09 00:41:45 +0100

    classes commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
+"""
+Square Module
+
+This module defines the Square class, which represents a square with a given size.
+The class provides methods to interact with and manipulate square objects.
+
+Classes:
+    Square
+
+Methods:
+    __init__(self, size)
+
+"""
 class Square: 
       """  
-        Represents a square with a given size
+        Represents a square with a given size.
+
          Attributes: 
              __size (int): The size of square.
         
@@ -19,3 +33,10 @@ class Square:
             None
         """
         self.__size = size 
+
+square1 = Square(0-Square)
+square2 = Square(7)
+
+print(square1._Square__size)
+print(square2._Square__size)     
+
