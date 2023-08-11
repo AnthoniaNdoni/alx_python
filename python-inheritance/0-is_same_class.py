3b90b7e5841e58fedba8e33631b594742d6df808
AnthoniaNdoni <125657089+AnthoniaNdoni@users.noreply.github.com>
2023-08-11 11:42:35 -0400

    Update 0-is_same_class.py

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
     return type(obj) is a_class
-
-a = 1
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
