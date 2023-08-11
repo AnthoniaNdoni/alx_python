6e1cf9ba572f339aa77eb3e5f002f54abb02f6ec
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-11 14:16:00 +0100

    editng commit

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-def is_kind_of_class(obj, a_class):
+
+def is_same_class(obj, a_class):
     """
-    Check if an object is an instance of or inherited from a specified class.
+    Check if an object is exactly an instance of the specified class.
+
+    This function compares the type of the given object (obj) with the
+    specified class (a_class) and returns True if they match exactly,
+    indicating that the object is an instance of the specified class.
+    Otherwise, it returns False.
 
     Args:
-        obj: The object to check.
-        a_class: The class to compare against.
+        obj (object): The object to be checked.
+        a_class (type): The class to compare against.
 
     Returns:
-        True if obj is an instance of a_class or inherited from it, False otherwise.
+        bool: True if obj is an instance of a_class, False otherwise.
     """
-    return isinstance(obj, a_class)
+    return type(obj) is a_class
