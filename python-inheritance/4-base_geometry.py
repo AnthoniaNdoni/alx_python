d4a6fae3aaa725130c374f78b06d8da090d037eb
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-11 21:57:56 +0100

    improve Geometry commit

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    class BaseGeometry:
+    An empty class representing BaseGeometry.
+
+     """
+class BaseGeometry:
+    """
+    An empty class representing BaseGeometry.
+    """
+
+    def area(self):
+     """
+        Raises an Exception with the message area() is not implemented.
+
+        Args:
+            self: The object itself.
+
+        Returns:
+            None (Raises an Exception).
+    """
+    raise Exception("area() is not implemented")
