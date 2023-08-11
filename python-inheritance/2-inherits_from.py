2dbe2cc1ca325dbf2cf8ac1b74e3db34bcd6ad07
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-11 20:48:19 +0100

    only sub class of commit

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,19 @@
+def inherits_from(obj, a_class):
+    """
+    Checks if an object is an instance of a class that inherited (directly or indirectly)
+    from the specified class.
+
+    Args:
+    obj: The object to be checked.
+    a_class: The class to compare against.
+
+    Returns:
+    True if obj is an instance of a class that inherited from a_class, otherwise False.
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
+
+
+
+
+
+
