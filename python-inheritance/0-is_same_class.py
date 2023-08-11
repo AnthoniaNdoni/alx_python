3b637bc95114aa6c8520f9f4fa9ec63b7651835a
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-11 16:46:57 +0100

    editing  the commit

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -4,4 +4,4 @@ def is_same_class(obj, a_class):
 
 a = 1
 if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
+    print("{} {}".format(a, int.__name__))
