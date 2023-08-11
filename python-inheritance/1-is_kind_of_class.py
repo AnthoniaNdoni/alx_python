7bababe3ce94c62690048ec9a8a5fc8f0c3964f4
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-11 20:29:05 +0100

    same class or inherit from commit

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Defines a function is_kind_of_class that checks if an object is an instance
+of a specified class or a class that inherits from it.
+"""
+
+def is_kind_of_class(obj, a_class):
+    """
+    Checks if an object is an instance of, or if it's an instance of a class
+    that inherits from, the specified class.
+
+    Args:
+        obj: The object to be checked.
+        a_class: The class to compare against.
+
+    Returns:
+        True if obj is an instance of a_class or its subclass, otherwise False.
+    """
+    return isinstance(obj, a_class)
