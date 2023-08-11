b66a418f7de0594b0caef9e317cd41b0c760bd04
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-11 20:08:53 +0100

    same object func commit

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,6 +1,23 @@
-def is_same_class(obj, a_class):
-    return type(obj) is a_class
+#!/usr/bin/python3
+"""
+    def is_same_class(obj, a_class):
+    Check if an object is an instance of or inherited from a specified class.
 
-a = 1
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__), end="")
+    Args:
+    obj: The object to be checked.
+    a_class: The specified class.
+
+    Returns: True if object is an instance of the specified class, otherwise False.
+    """
+def is_same_class(obj, a_class):
+    """
+    def is_same_class(obj, a_class):
+    check if an object is an instance of a specified class
+
+    Args:
+    obj: The object to be checked.
+    a_class: The specified class.
+
+     Returns: True if object is an instance of the specified class, otherwise False.
+    """
+    return type(obj) is a_class
