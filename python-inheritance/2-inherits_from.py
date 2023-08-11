4db88f8e33817a9b1be88c72c6e324bf892640c4
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-11 20:56:06 +0100

    edited commit

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,3 +1,14 @@
+#!/usr/bin/python3
+"""
+    def inherits_from(obj, a_class):
+    Checks if an object is an instance of a class that inherited (directly or indirectly)
+    from the specified class.
+
+    Args:
+    obj: The object to be checked.
+    a_class: The class to compare against.
+     """
+
 def inherits_from(obj, a_class):
     """
     Checks if an object is an instance of a class that inherited (directly or indirectly)
