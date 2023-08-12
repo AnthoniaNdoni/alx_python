20ed7760e354607caf4712800c6c31579dae96c1
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-12 13:41:30 +0100

    Geometry module commit

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
+    BaseGeometry Module
     class BaseGeometry:
     An empty class representing BaseGeometry.   
     
     """
 class BaseGeometry:
     """
-    An empty class representing BaseGeometry.
+    An empty class representing Base Geometry.
     """
     pass
