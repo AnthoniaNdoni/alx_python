5bb41de5d8285a022c49422f478ca4fe825a91d3
AnthoniaNdoni <125657089+AnthoniaNdoni@users.noreply.github.com>
2023-08-15 13:43:30 -0400

    Update 6-rectangle.py

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -16,10 +16,6 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """
     Represents a rectangle, inheriting from BaseGeometry
-    
-    """
-def __init__(self, width, height):
-    """
     Initializes a new Rectangle instance with the specified width and height.
 
     Args:
@@ -28,6 +24,7 @@ def __init__(self, width, height):
 
     Returns:None
     """
+def __init__(self, width, height):
     self.integer_validator("width", width)
     self.integer_validator("height", height)
     self.__width = width
