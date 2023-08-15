b7df50efbef78c3bba4c27f95d96ac55c17e4a14
AnthoniaNdoni <125657089+AnthoniaNdoni@users.noreply.github.com>
2023-08-15 14:21:31 -0400

    Update 6-rectangle.py

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-Defines a class Rectangle that inherits from BaseGeometry
+    Defines a class Rectangle that inherits from BaseGeometry:
 
-this module Defines a class Rectangle that inherits from BaseGeometry
+    this module Defines a class Rectangle that inherits from BaseGeometry
 
-class Rectangle(BaseGeometry):
+    class Rectangle(BaseGeometry):
     Initializes a new Rectangle instance with the specified width and height.
     """
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
-    """Represents a rectangle, inheriting from BaseGeometry
-    
+    """Represents a rectangle, inheriting from BaseGeometry:
+
     Initializes a new Rectangle instance with the specified width and height.
 
 Args:
