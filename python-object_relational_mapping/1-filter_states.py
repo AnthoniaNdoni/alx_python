66b6b4d4730a9170290f9a9c1fb8e9b12a541707
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-23 15:42:12 +0100

    First initial commit

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#Lists all states from the database hbtn_0e_0_usa.
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
+import sys
+import MySQLdb
+
+try:
+    if len(sys.argv)> 3:
+        database = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        cursor =database.cursor()
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC") 
+        rows= cursor.fetchall()
+        for state in rows:
+            print(state)
+        database.close()
+    else:
+        None
+except MySQLdb.OperationalError as e:
+    print("connection failed. {}".format(e))
