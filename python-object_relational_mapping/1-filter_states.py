d9593ad0206f72b37d30b3a542625ad0659f8a05
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-23 19:52:43 +0100

    correcting  initial commit

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Lists all states from the database hbtn_0e_0_usa.
- Usage: ./0-select_states.py <mysql username> \
+Usage: ./0-select_states.py <mysql username> \
                              <mysql password> \
                             <database name>
 """
@@ -12,8 +12,9 @@ try:
     if len(sys.argv) > 3:
         database = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
         cursor =database.cursor()
-        cursor.execute("SELECT * FROM states WHERE name LIKE N%"
-                       "ORDER BY id ASC")
+        cursor.execute(
+             "SELECT * FROM states WHERE name LIKE N% ORDER BY id ASC"
+            )
         rows= cursor.fetchall()
         for state in rows:
             print(state)
@@ -21,4 +22,4 @@ try:
     else:
         None
 except MySQLdb.OperationalError as e:
-    print("connection failed. {}".format(e))
+        print("connection failed. {}".format(e))
