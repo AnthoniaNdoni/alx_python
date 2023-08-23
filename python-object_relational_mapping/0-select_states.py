d8eb3998cb8d0967e423343f2dd4210c65a3a500
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-23 18:35:27 +0100

    correcting initial commiting

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,16 +4,15 @@ Lists all states from the database hbtn_0e_0_usa.
  Usage: ./0-select_states.py <mysql username> \
                             <mysql password> \
                              <database name>
- """                            
+"""                            
 import sys
 import MySQLdb
 
 try:
     if len(sys.argv) > 3:
-        database = MySQLdb.connect(user=sys.argv[1], \
-                                    passwd=sys.argv[2], db=sys.argv[3])
+        database = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
         cursor =database.cursor()
-        cursor.execute("SELECT * FROM states") 
+        cursor.execute("SELECT * FROM states")
         rows= cursor.fetchall()
         for state in rows:
             print(state)
