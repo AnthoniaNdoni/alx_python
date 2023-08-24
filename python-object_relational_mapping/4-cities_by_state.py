ae848e5b89ed566c164450ec8758cb23ae61efbb
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-25 00:18:18 +0100

    correcting initial commit

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,6 @@
 """ 
-A script that takes in the name of a state 
-as an argument and lists all cities of that state,
-using the database hbtn_0e_4_usa
+A script that lists all cities from the database hbtn_0e_4_usa
+
 """ 
 
 # import dbobject
