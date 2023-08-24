ddc9efa75675fbccbc8991b99185c545825546f2
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-25 00:15:27 +0100

    initial commit for task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,43 @@
+""" 
+A script that takes in the name of a state 
+as an argument and lists all cities of that state,
+using the database hbtn_0e_4_usa
+""" 
+
+# import dbobject
+import MySQLdb
+import sys
+
+# try connection and execution
+try:
+    # connect to database
+
+    if len(sys.argv) > 3:
+        database = MySQLdb.connect(user=f"{sys.argv[1]}",
+                                   passwd=f"{sys.argv[2]}",
+                                   db=f"{sys.argv[3]}")
+
+        # set cursor if connection succed
+        cursor = database.cursor()
+
+        # run the select statement on the cities table
+        cursor.execute("SELECT cities.id, cities.name, \
+                        states.name FROM cities INNER JOIN \
+                        states ON cities.state_id=states.id \
+                        ORDER by cities.id")
+
+        # fetch all rows in the result
+        rows = cursor.fetchall()
+
+        # loop through the result to get the cities id and name
+        for row in rows:
+            print(row)
+    else:
+        pass
+
+    database.close()
+    # if there is an error catch it with and exception message
+except MySQLdb.OperationalError as e:
+
+    # print the error message
+    print("Connection failed. {}".format(e))
