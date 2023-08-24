3641738928b8d1f09f7a1928d9e783e5d9cfa830
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-24 01:19:56 +0100

    commit

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,24 +5,32 @@ Lists all states from the database hbtn_0e_0_usa.
                             <mysql password> \
                              <database name>
 """
+# import dbobject
 import sys
 import MySQLdb
 
 try:
+    # connect to the database
     if len(sys.argv) > 3:
         database = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
         cursor =database.cursor()
 
         # Corrected line length and added spaces around operators
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        
-        rows= cursor.fetchall()
-        for state in rows:
-            print(state)
+        cursor.execute("SELECT * FROM states ORDER by states.id")
 
-        cursor.cose() # move this line insde the if block
-        database.close()
+        # fetch all rows in the result
+        rows= cursor.fetchall()
+
+        # loop through the result to get the state id and name
+        for row in rows:
+            print(row)
+
     else:
-        None
+        pass
+
+    database.close()
+    # if there is an error catch it with and exception message
 except MySQLdb.OperationalError as e:
+
+    # print the error message
     print("Connection failed. {}".format(e))
