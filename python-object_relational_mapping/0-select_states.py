cfd5dc01d9dcecd0d6524f7fb904ecfd630d1e1c
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-24 09:38:34 +0100

    commit

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -13,9 +13,9 @@ try:
     # connect to the database
     if len(sys.argv) > 3:
         database = MySQLdb.connect(user=f"{sys.argv[1]}",
-                                    passwd=f"{sys.argv[2]}", 
-                                    db=f"{sys.argv[3]}")
-        
+                                   passwd=f"{sys.argv[2]}",
+                                   db=f"{sys.argv[3]}")
+
         # set cursor if connection succed
         cursor = database.cursor()
 
