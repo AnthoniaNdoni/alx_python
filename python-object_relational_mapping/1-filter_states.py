b504aed780ee619b5583ac62579316d4cf32dcd6
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-24 11:02:26 +0100

    commit

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Lists all states with a name starting with N(Upper N) from database hbtn_0e_0_usa.
+Lists all states with a name starting with N(Upper N)from db hbtn_0e_0_usa.
 Usage: ./0-select_states.py <mysql username> \
-                             <mysql password> \
+                            <mysql password> \
                             <database name>
 """
 
@@ -12,13 +12,13 @@ import MySQLdb
 
 # try connction and execution
 try:
-    # connect to database
 
+    # connect to database
     if len(sys.argv) > 3:
         database = MySQLdb.connect(user=f"{sys.argv[1]}",
                                    passwd=f"{sys.argv[2]}",
                                    db=f"{sys.argv[3]}")
-        
+
         # set cursor if connection succeed
         cursor = database.cursor()
 
@@ -33,14 +33,14 @@ try:
         # loop to get the id and name
         for row in rows:
 
-            # print only the capital letter 
+            # print only the capital letter
             print(row) if row[1][0] == 'N' else None
-        
+
         database.close()
     else:
-        None
+        pass
 
-    # if there is an error catch it with exception message 
+    # if there is an error catch it with exception message
 except MySQLdb.OperationalError as e:
 
     # print the error message
