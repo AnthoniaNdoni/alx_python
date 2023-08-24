594badc81624679f0deaf27293f93c5c8b10984a
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-24 22:13:10 +0100

    commit

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,18 +10,20 @@ import MySQLdb
 
 try:
     if len(sys.argv) > 4:
-        database = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-        cursor =database.cursor()
+        database = MySQLdb.connect(user=f"{sys.argv[1]}",
+                                   passwd=f"{sys.argv[2]}",
+                                   db=f"{sys.argv[3]}")
+        cursor = database.cursor()
         state_name = sys.argv[4]
         cursor.execute("SELECT * FROM states WHERE BINARY name = %s "
                        "ORDER BY id ASC", (state_name,))
-        
-        rows= cursor.fetchall()
+
+        rows = cursor.fetchall()
         for state in rows:
             print(state)
-            
+
         database.close()
     else:
-        None
+        pass
 except MySQLdb.OperationalError as e:
     print("connection failed. {}".format(e))
