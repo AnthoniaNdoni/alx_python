47bee6272e98211d2d99fd5c5a91bbdbe1a79b58
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-27 23:04:25 +0100

    commit

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,14 +1,23 @@
+# Import necessary SQLAIchemy components
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
+# Create an engine to connect to the MySQL server
+# Replace "mysql+mysqldb://root:root@localhost/food_db" with your connection URL
+# The URL format is "mysql+mysqldb://username:password@host/database_name"
 # Connecting to MySQL server at 23.92.23.113 using mysql-python DBAPI 
 engine = create_engine("mysql+mysqldb://root:root@localhost/food_db")
 
+# create a session factory
 Session = sessionmaker(bind = engine)
 session = Session()
 
+# Connect to the database using the engine
 connection = engine.connect()
+
+# check if the connction was successful
 if connection:
     print("Database is connected") 
 else:
