f0d25cbedd4f673e3dab896c7fde277243009476
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-28 19:56:02 +0100

    First initial commit

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,22 +1,28 @@
+"""
+Write a python file that contains the class definition of 
+a State and an instance Base = declarative_base()
+"""
 # Import necessary SQLAIchemy components
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# define Base class for table class inheritance
+Base = declarative_base()
 
-# Create an engine to connect to the MySQL server
-# Connecting to MySQL server at 23.92.23.113 using mysql-python DBAPI 
-engine = create_engine("mysql+mysqldb://root:root@localhost/food_db")
+# creat s table for class state
+class state(Base):
+    """
+    class definition for sql table states
+    PARAMETER
+        Base declarative
+    Return:
+    base meta data for the creation of state tables
+    """
+   # define table name
+    __tablename__ = 'states'
+    # define column for states table
+    id = Column(Integer, primary_key=True,
+                autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
 
-# create a session factory
-Session = sessionmaker(bind = engine)
-session = Session()
-
-# Connect to the database using the engine
-connection = engine.connect()
-
-# check if the connction was successful
-if connection:
-    print("Database is connected") 
-else:
-    print("database is not connected")
