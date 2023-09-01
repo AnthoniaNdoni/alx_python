b31252c3d473989880bf70afbe396113252c7d0b
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-01 19:45:35 +0100

    initial commit

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -16,11 +16,20 @@ class BaseGeometry:
     """
 
 def area(self):
-    """
-    Raises an Exception with the message area() is not implemented.
-    Args:
-    self: The object itself.
 
-    Returns:None (Raises an Exception).
-    """
-    raise Exception("area() is not implemented")
+    def __dir__(cls) -> None:
+        """
+        The function overide the default __dir__ function    
+        """
+        attrib = super().__dir__()
+        n_attri = []
+        for attr in attrib:
+            if attr != '__init_subclass__':
+                n_attri.append(attr)
+        return n_attri
+
+    def area(self):
+        """
+        This function raise an error if the area method is not define
+        """
+        raise Exception("area() is not implemented")
