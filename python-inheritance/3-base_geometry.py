8fe7d6a5c3717e26042adbb37ae84a2adc4eb17b
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-01 19:39:33 +0100

    correcting commit

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -3,8 +3,27 @@
     BaseGeometry Module
     """
 
-class BaseGeometry:
+class BaseGeometryMetaClass(type):
     """
-    An empty class representing Base Geometry.
+    An metaclass for Base Geometry.
     """
-    pass
+    def __dir__(cls)->None:
+        """
+        A function define to remove the __init_subclass__ from dir
+        """
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    
+class BaseGeometry(metaclass=BaseGeometryMetaClass):
+    """
+    this method check if a class is an instance of the define object
+    exam 1 is an instancce of the class int
+    """
+
+    def __dir__(cls) -> None:
+
+        attrib = super().__dir__()
+        n_attri = []
+        for attr in attrib:
+            if attr != '__init_subclass__':
+                n_attri.append(attr)
+        return n_attri
