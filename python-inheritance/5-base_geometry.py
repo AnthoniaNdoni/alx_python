6ccfba550a29d0d9332a37c0b40806a4367f47a4
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-01 20:02:44 +0100

    initial commit

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""
-    class BaseGeometry:
-    An empty class representing BaseGeometry.
 
-     """
+#!/usr/bin/python3
+"""
+The module of a class name Geometry that has a single function
+"""
+
+
 class BaseGeometry:
     """
-    An empty class representing BaseGeometry.
+    this method check if a class is an instance of the define object
+    exam 1 is an instancce of the class int
     """
-def __dir__(cls) -> None:
+
+    def __dir__(cls) -> None:
         """
         this method check if a class is an instance of the define object
             exam 1 is an instancce of the class int
@@ -20,12 +24,11 @@ def __dir__(cls) -> None:
                 n_attri.append(attr)
         return n_attri
 
-def area(self):
+    def area(self):
         raise Exception("area() is not implemented")
 
-def integer_validator(self, name:str, value:int):
+    def integer_validator(self, name:str, value:int):
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
