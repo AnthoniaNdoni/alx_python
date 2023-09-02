5855c5d6c3f62dd3c7fa078bd7d2cb11f539c14c
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-02 22:58:18 +0100

    commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,190 +1,283 @@
-#!/usr/bin/python3
 """
-module documentation
+    This is the first Rectangle that 
+    inherits from Base Module
 """
-
 from models.base import Base
+# from base import Base
 
-class Rectangle(Base): 
+class Rectangle(Base):
     """
-     Rectangle class that inherits from the Base class.
-     Attributes:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-        x (int): The x-coordinate of the rectangle.
-        y (int): The y-coordinate of the rectangle.
-        id (int): The unique identifier of the instance.
+    Definition:
+        A class defition for Rectangle that takes private param,
+        and initiialize using a constructior
+    Inheritance:
+        inherit from Base class.
     """
+
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-        Class constructor.
-     Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-            x (int, optional): The x-coordinate of the rectangle. Defaults to 0.
-            y (int, optional): The y-coordinate of the rectangle. Defaults to 0.
-            id (int, optional): The id for the instance. Defaults to None
+        Class construtuctor for the clas rectangle
+
+        Parameters:
+            width: supplied at instatiation
+            height: supplied at instatiation
+            x:  default = 0
+            y:  default = 0
+            id: default = None, inherited from Base class
+        Return:
+            The return param will be
+
         """
+        # validate each param
+        self.validation('width', width)
+        self.validation('height',height)
+        self.validation('x',x)
+        self.validation('y',y)
+
+        # set each param after validation
+        self._Rectangle__width = width
+        self._Rectangle__height = height
+        self._Rectangle__x = x
+        self._Rectangle__y = y
         super().__init__(id)
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        elif width <= 0:
-            raise ValueError("width must be > 0")
+
+    def __str__(self):
+        """
+        The string method takes the object and return a readable
+        format 
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+            self._Rectangle__x, self._Rectangle__y, self._Rectangle__width,
+                self._Rectangle__height)
+
+
+    #   @property decorator. This method allows us to
+    #   access the private attribute as if it
+    #   were a regular attribute, using obj.private_attribute
+    @property
+    def Rectangle__width(self):
+        '''
+        Return:
+            return the width param from the private property
+        '''
+        return self._Rectangle__width
+
+    #    This method allows us to set the private attribute's
+    #    value with validation or additional logic
+    @Rectangle__width.setter
+    def Rectangle__width(self, width):
+        '''
+        Return:
+           Set the width private property attributes field.
+        '''
+        self.validation('width', width)
+        self._Rectangle__width = width
+
+    @property
+    def width(self):
+        '''
+        Return:
+            return the width param from the private property
+        '''
+        return self._Rectangle__width
+    
+    @width.setter
+    def width(self, width):
+        '''
+        Return:
+           Set the width private property attributes field.
+        '''
+        self.validation('width', width)
+        self._Rectangle__width = width
+
+
+    @property
+    def Rectangle__height(self):
+        '''
+        Return:
+            return the height param from the private property
+        '''
+        return self._Rectangle__height
+
+    @Rectangle__height.setter
+    def Rectangle__height(self, height):
+        '''
+        Return:
+           Set the height private property attributes field.
+        '''
+        self.validation('height', height)
+        self._Rectangle__height = height
+
+    @property
+    def height(self):
+        '''
+        Return:
+            return the height param from the private property
+        '''
+        return self._Rectangle__height
+    
+    @height.setter
+    def height(self, height):
+        '''
+        Return:
+           Set the height private property attributes field.
+        '''
+        self.validation('height', height)
+        self._Rectangle__height = height
+
+    @property
+    def Rectangle__x(self):
+        '''
+        Return:
+            return the x param from the private property
+        '''
+        return self._Rectangle__x
+
+    @Rectangle__x.setter
+    def Rectangle__x(self, x):
+        '''
+        Return:
+           Set the x private property attributes field.
+        '''
+        self.validation('x', x)
+        self._Rectangle__x = x
+
+    @property
+    def x(self):
+        '''
+        Return:
+            return the x param from the private property
+        '''
+        return self._Rectangle__x
+    
+    @x.setter
+    def x(self, x):
+        '''
+        Return:
+           Set the x private property attributes field.
+        '''
+        self.validation('x', x)
+        self._Rectangle__x = x
+
+    @property
+    def Rectangle__y(self):
+        '''
+        Return:
+            return the x param from the private property
+        '''
+        return self.__y
+
+    @Rectangle__y.setter
+    def Rectangle__y(self, y):
+        '''
+        Return:
+           Set the y private property attributes field.
+        '''
+        self.validation('y', y)
+        self._Rectangle__y = y
+
+    @property
+    def y(self):
+        '''
+        Return:
+            return the y param from the private property
+        '''
+        return self._Rectangle__y
+    
+    @y.setter
+    def y(self, y):
+        '''
+        Return:
+           Set the y private property attributes field.
+        '''
+        self.validation('y', y)
+        self._Rectangle__y = y
+
+    #   validation class for Rectangle
+
+    def validation(self, name:str, param:int):
+        '''
+        Validation method 
+
+        Parameters:
+            Name: str name of param to be vailidated. example x
+            Param: int, the value to be valitade must be int at
+            all time
+        Return:
+            exception: Typeerror, ValueError or Pass if test is 
+            successful
+        '''
+        if(type(param) != int):
+            raise TypeError("{} must be an integer".format(name))
+        elif(type(name) != str):
+            raise ValueError("{} name must be a str".format(name))
+        elif(name not in ['x','y']):
+            if(param <= 0):
+                raise ValueError("{} must be > 0".format(name))
+        elif(name == 'x' or name == 'y'):
+            if(param < 0):
+                raise ValueError("{} must be >= 0".format(name))
+
+    #   Area calculatore for Rectangle class
+
+    def area(self):
+        '''
+        THis method return the area of the the rectangle
+        PArameters:
+            self
+        Return:
+            the prpduct of height and width
+        '''
+        return self._Rectangle__height * self._Rectangle__width
+
+    #  Display method for python class Rectangle
+
+    def display(self):
+        '''
+        This function return a square matrix of # for anyinput of x and y
+
+        Paramter:
+            self:
+        Return:
+            square matrix of #
+        '''
+        width = self._Rectangle__width
+        height = self._Rectangle__height
+        x = self.Rectangle__x
+        y = self.Rectangle__y
+
+        # print a square matrix from x and y
+        for i in range(y): # write the top space
+            print("" * y, end = '\n')
+        for i in range(height): # row
+            for j in range(x):  # write the column white space
+                print(" ", end = '')
+            for j in range(width-1): #column
+                print("#", end = "")
+            print("#")
+
+        #   Update for class python
+    def update(self, *args, **kwargs):
+        '''
+        This function return a square matrix of # for anyinput of x and y
+
+        Paramter:
+            self:
+        Return:
+            square matrix of #
+        '''
+        if args:
+            for arg in args:
+                self.id = arg 
+                self._Rectangle__width = arg
+                self._Rectangle__height = arg
+                self._Rectangle__x = arg
+                self._Rectangle__y = arg
         else:
-            self.__width = width
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        elif height <= 0:
-             raise ValueError("height must be > 0")
-        else:    
-            self.__height = height
-        if type(x) is not int:
-            raise TypeError("x must be an integer")
-        elif x < 0:
-            raise ValueError("x must be >= 0")
-        else:    
-            self.__x = x
-        if type(y) is not int:
-            raise TypeError("y must be an integer")
-        elif y < 0:
-            raise ValueError("y must be >= 0")
-        else:    
-            self.__y = y
-        
-        @property
-        def width(self):
-            """ 
-            Get the width of the rectangle.
-            """
-            return self.__width
-        
-        @width.setter
-        def width(self, value):
-            """ 
-            Set the width of the rectangle.
-            """
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            elif value <= 0:
-                raise ValueError("width must be > 0")
-            else:
-                self.__width = value
-
-        @property
-        def height(self):
-            """
-            Get the height of the rectangle.
-            """
-            return self.__height
-
-        @height.setter
-        def height(self,  value):
-            """
-            Set the height of the rectangle.
-            """
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            elif value <= 0:
-                raise ValueError("height must be > 0")
-            else:
-                self.__height = value
-
-        @property
-        def x(self):
-            """
-            Get the x-coordinate of the rectangle.
-            """
-            return self.__x
-
-        @x.setter
-        def x(self, value):
-            """
-            Set the x-coordinate of the rectangle.
-            """
-            if type(value) is not int:
-                raise TypeError("x must be an integer")
-            elif value < 0:
-                raise ValueError("x must be >= 0")
-            else:
-                self.__x = value
-
-        @property
-        def y(self):
-            """
-            Get the y-coordinate of the rectangle.
-            """
-            return self.__y
-
-        @y.setter
-        def y(self, value):
-            """
-            Set the y-coordinate of the rectangle.
-            """
-            if type(value) is not int:
-                raise TypeError("y must be an integer")
-            elif value < 0:
-                raise ValueError("y must be >= 0")
-            else: 
-                self.__y = value
-
-        def area(self):
-            """
-            calculate and return the area of the rectangle
-            """ 
-            return self.width * self.height 
-
-        def display(self):
-            """
-            Display the rectangle using the '#' character.
-            """
-            for row in range(self.y):
-                print()
-            else:
-                for row in range(self.height):
-                    for column in range(self.width):
-                        print(" ",end="")
-                else:
-                    for column in range(self.width):
-                        print("#", end="")
-                    else:    
-                        print()  
-
-        def __str__(self):
-            """
-            str update
-            """
-            return "[Rectangle]({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
-        
-        def update(self, *args, **kwargs):
-            """
-            update attributes
-            """
-
-            args_length = len(args)
-            kwargs_length = len(kwargs)
-
             for key, value in kwargs.items():
-                if key == "id":
-                    self.id= value
-                elif key == "width":
-                 self.width = value
-                elif key == "height": 
-                    self.height = value
-                elif key == "x":
-                    self.x = value
-                elif key == "y":
-                    self.y = value  
-
-                     
-
-            if args_length > 0:
-                self.id = args[0]
-            if args_length > 1:
-                self.width = args[1]
-            if args_length > 2:
-                self.height = args[2]
-            if args_length > 3:
-                self.x = args[3]
-            if args_length > 4:
-                self.y  = args[4]                    
+                self.id = kwargs.get('id') if 'id' in kwargs else self.id
+                self._Rectangle__x = kwargs.get('x') if 'x' in kwargs else self.Rectangle__x
+                self._Rectangle__y = kwargs.get('y') if 'y' in kwargs else self.Rectangle__y
+                self._Rectangle__width = kwargs.get('width') if 'width' in kwargs else self.Rectangle__width
+                self._Rectangle__height = kwargs.get('height') if 'height' in kwargs else self.Rectangle__height
--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,39 +1,39 @@
-"""
-module documentation
-"""
-from models.rectangle import Rectangle 
+'''
+This class implement the square method based and also inherit from the 
+Rectanngle class we previously built
+'''
+
+# from rectangle import Rectangle
+from models.rectangle import Rectangle
+# 
 
 class Square(Rectangle):
-    """
-    Square class that inherits from the Rectangle class.
-    
-    Attributes:
-        size (int): The size of the square (same as width and height).
-        x (int): The x-coordinate of the square.
-        y (int): The y-coordinate of the square.
-        id (int): The unique identifier of the instance.
-    """
-
+    '''
+    this is a class that inherit from the rectangle class
+    '''
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Class constructor.
-        ... (rest of the __init__ method) ...
-
-    ... (other methods) ...
-
-    def update(self, *args, **kwargs)
-
-        Args:
-            size (int): The size of the square (same as width and height).
-            x (int, optional): The x-coordinate of the square. Defaults to 0.
-            y (int, optional): The y-coordinate of the square. Defaults to 0.
-            id (int, optional): The id for the instance. Defaults to None.
-        """
-        super().__init__(size, size, x, y, id)  # Call the constructor of the Rectangle class
-        self.size = size
-
-    # Additional methods, if needed...
-
+        '''
+        instantation of the square class
+        '''
+        super().__init__(width=size,height=size, x=x,y=y, id=id)
+        
     def __str__(self):
-        """Return a string representation of the Square."""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
+        '''
+        This method convert the objects to strings
+        '''
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        '''
+        This method is defined to get te size of the object
+        '''
+        return super().Rectangle__width
+
+    @size.setter
+    def size(self, size):
+        '''
+        THis is a setter method for size
+        '''
+        Rectangle.validation(self, 'width', size)
+        super().__init__(size, size)
